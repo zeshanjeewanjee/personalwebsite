42dd4ccd464379ba1cdc3e40b0e4938ba9ab764a
zeshanjeewanjee <31360419+zeshanjeewanjee@users.noreply.github.com>
2017-09-11 10:54:21 -0700

    Haeder PHP Link Update

--- includes/links.php
+++ includes/links.php
@@ -3,5 +3,5 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://necolas.github.io/normalize.css/latest/normalize.css">
 <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
-<link rel="stylesheet" href="<?php echo $conf['fpfix']?>css/bsstyles.css">
+<link rel="stylesheet" href="/css/bsstyles.css">
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
